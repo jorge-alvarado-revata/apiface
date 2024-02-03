d77c2941a96bfada364edc20f74db3077b09a202
died_sistemas12 <died_sistemas12@minedu.gob.pe>
2024-02-02 22:16:37 -0500

    add compare images

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Set, Union
+from typing import Set, Union, Annotated
+import numpy as np
+
+import cv2
+
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Image(BaseModel):
     name: str
@@ -26,3 +45,24 @@ async def post_compasison(item: ComparaImage):
 async def face_compasison():
     results = {"status_code":200, "status_result": "OK." }
     return results
+
+@app.post("/comparison_img/")
+async def create_file(
+    fileA: Annotated[UploadFile, File()],
+    fileB: Annotated[UploadFile, File()]):
+    
+    imageA =  fileA.file.read()
+
+    imageB =  fileB.file.read()
+
+    imgA_cv2 = cv2.imdecode(np.frombuffer(imageA, np.uint8), cv2.IMREAD_UNCHANGED)
+
+    imgB_cv2 = cv2.imdecode(np.frombuffer(imageB, np.uint8), cv2.IMREAD_UNCHANGED)
+
+
+
+    return {
+        "file_sizeA": imgA_cv2.shape,
+        "file_sizeB": imgB_cv2.shape,
+        "eval": 0.0
+    }
