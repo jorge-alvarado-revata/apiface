4e415d6a58f15810249376b3d2f621619e911f98
died_sistemas12 <died_sistemas12@minedu.gob.pe>
2024-02-03 19:40:04 -0500

    update compare image#

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# syntax=docker/dockerfile:1
+FROM python:3.11.3
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+WORKDIR /code
+COPY ./requirements.txt /code/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+COPY ./app /code/app
+CMD ["uvicorn", "app.main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import Set, Union, Annotated
+import face_recognition
+import logging
+from fastapi.encoders import jsonable_encoder
 
 import numpy as np
 
@@ -10,11 +13,13 @@ import base64
 
 import cv2
 
-
-
 app = FastAPI()
 
 origins = [
+    "http://0.0.0.0",
+    "http://0.0.0.0:80",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8080",
     "http://localhost",
     "http://localhost:8080",
 ]
@@ -26,8 +31,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Image(BaseModel):
     name: str
@@ -52,6 +55,12 @@ async def face_compasison():
     results = {"status_code":200, "status_result": "OK." }
     return results
 
+def find_face_encoding(image):
+    face_enc = face_recognition.face_encodings(image)
+    return face_enc[0]
+
+
+
 @app.post("/comparison_img/")
 async def create_file(
     fileA: Annotated[UploadFile, File()],
@@ -65,14 +74,31 @@ async def create_file(
 
     imgB_cv2 = cv2.imdecode(np.frombuffer(imageB, np.uint8), cv2.IMREAD_UNCHANGED)
 
-    gray_imgA = cv2.cvtColor(imgA_cv2, cv2.COLOR_BGR2GRAY)
+    gray_imgA = cv2.cvtColor(imgA_cv2, cv2.COLOR_BGR2RGB)
 
-    gray_imgB = cv2.cvtColor(imgB_cv2, cv2.COLOR_BGR2GRAY)
+    gray_imgB = cv2.cvtColor(imgB_cv2, cv2.COLOR_BGR2RGB)
+
+    image_1  = find_face_encoding(gray_imgA)
+
+    image_2 = find_face_encoding(gray_imgB)
+
+    #check si son las mismas personas
+
+    is_same = face_recognition.compare_faces([image_1], image_2)[0]
+
+    
+    logging.warning('is_same', is_same)
+
+    accuracy = 0.0
+    if is_same:
+        distance = face_recognition.face_distance([image_1], image_2)
+        distance = round(distance[0] * 100)
+        accuracy = 100 - round(distance)
+
 
     return {
-        "file_sizeA": imgA_cv2.shape,
-        "file_sizeB": imgB_cv2.shape,
-        "eval": 0.0
+        "same": f"{is_same}",
+        "accuracy": f"{accuracy}"
     }
 
 
@@ -93,6 +119,8 @@ async def create_file(
         gray_imgA, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
     )
 
+    has_human = len(face)
+
     for (x, y, w, h) in face:
         cv2.rectangle(gray_imgA, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
@@ -105,5 +133,6 @@ async def create_file(
     return {
         "file_sizeA": imgA_cv2.shape,
         "eval": 0.0,
-        "content": encoded_img
+        "content": encoded_img,
+        "human": has_human
     }
