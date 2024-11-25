26482fbd93cce1693a423e8587a8bf927a0f8bea
Jorge Alvarado <j.revatta@gmail.com>
2024-11-25 13:38:06 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # apiface
+
+Demostración de uso de opencv en un Endpoint FastAPI para 
+la detección de rostros con OPENCV.
+
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
